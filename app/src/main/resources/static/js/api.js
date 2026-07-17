3a5879cb05b0a032c5559813f5689d608b40200f
rathishkumar31 <720823106090@hit.edu.in>
2026-07-17 18:05:05 +0530

    updated

--- app/src/main/resources/static/js/api.js
+++ app/src/main/resources/static/js/api.js
@@ -79,9 +79,9 @@ export const categoryService = {
 // Supplier Services
 export const supplierService = {
   getAll: () => api.get('/suppliers'),
-  getById: (id) => api.get(`/suppliers/${id}`),
+  getById: (id) => api.get(`/suppliers/${id}),
   create: (data) => api.post('/suppliers', data),
-  update: (id, data) => api.put(`/suppliers/${id}`, data),
+  update: (id, data) => api.put(`/suppliers/${id}, data),
   delete: (id) => api.delete(`/suppliers/${id}`),
 };
 
